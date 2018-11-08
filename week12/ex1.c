2d9d30fa2ee6d86940195dbce16d464d5ae7dcee
elukyanchikova <31773897+elukyanchikova@users.noreply.github.com>
2018-11-08 17:06:04 +0300

    Add files via upload

---
+++ week12/ex1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main() {
+    int f = open("random.txt", O_RDONLY);
+    if (f == -1) {
+        printf("Error");
+    } else {
+        int length = 20;
+        char output[length + 1];
+        int r = read(f, output, length);
+        if (r != -1) {
+            printf("%s", output);
+
+        } else {
+            printf("r");
+
+        }
+    }
+    close(f);
+    return 0;
+}
